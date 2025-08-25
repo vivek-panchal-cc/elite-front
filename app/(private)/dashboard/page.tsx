9d73c97dc35409c830f3f8bd43fb7c92a421162f
yashk2023 <yash.k@communicationcrafts.com>
2025-08-25 13:25:17 +0530

    - Add bottom stripe

--- app/(private)/dashboard/page.tsx
+++ app/(private)/dashboard/page.tsx
@@ -1,13 +1,21 @@
 "use client";
 
+import Image from "next/image";
 import LatestOffer from "./components/LatestOffer";
 // import PrivateLayout from "../PrivateLayout";
 import ProfileDashboard from "./components/ProfileHeader";
+import { images } from "@/components/images";
 const Dashboard = () => {
   return (
     <>
       <ProfileDashboard />
       <LatestOffer />
+      <div className="bg-[#D9D9D9] flex-wrap lg:h-[114px] flex items-center justify-center p-3 gap-6">
+        <Image src={images.stokeOffer} alt="img" />
+        <Image src={images.stokeOffer} alt="img" />
+        <Image src={images.stokeOffer} alt="img" />
+        <Image src={images.stokeOffer} alt="img" />
+      </div>
       {/* <PrivateLayout>
       </PrivateLayout> */}
     </>
