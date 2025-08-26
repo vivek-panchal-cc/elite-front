5908b22cd9d3e252bab9b1a43a6a5e0ce8f59b90
yashk2023 <yash.k@communicationcrafts.com>
2025-08-26 18:33:03 +0530

    - Integrate transfer page

---
+++ app/(private)/transfer/components/PaypalModal.tsx
@@ -0,0 +1,135 @@
+import { images } from "@/components/images";
+import { Button } from "@/components/ui/ButtonUI";
+import { Input } from "@/components/ui/Input";
+import { Label } from "@/components/ui/Label";
+import Modal from "@/components/ui/Modal";
+import Image from "next/image";
+import React from "react";
+
+const PaypalModal = ({isOpen, handleClose}: {isOpen: boolean, handleClose: () => void}) => {
+  return (
+    <Modal isOpen={isOpen} onClose={handleClose} classStyle="" isClose={false}>
+      {/* border-[1.5px] border-[var(--color-red)] */}
+      <div className="relative h-[96px] text-white bg-[#10499E] text-center flex items-center justify-center">
+        <h4 className="text-[28px] md:text-[34px] font-bold">PAYPAL</h4>
+        <Button
+          variant="ghost"
+          size="icon"
+          className="mt-[10px] mr-[10px] border text-white h-[20px] w-[20px] rounded-[50px] absolute top-2 right-2 right-2 text-2xl"
+          onClick={handleClose}
+          aria-label="Close"
+        >
+          &times;
+        </Button>
+      </div>
+      <div className="p-5 md:px-13 md:py-8 max-w-[809px]">
+        <div className={`grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-x-4 md:gap-y-8`}>
+          <div className="space-y-1">
+            <Label className="font-bold text-sm sm:text-base md:text-base text-[var(--color-black)]">
+              Available Balance
+            </Label>
+            <Input
+              className="w-[100%] lg:w-[334px] bg-[var(--color-white)] border border-[var(--color-red)] focus-visible:border-[var(--color-red)] focus-visible:ring-1 focus-visible:ring-[var(--color-red)] placeholder:text-[14px]"
+              // value={formCompany.companyName}
+              placeholder="Available Balance"
+              // onChange={(e) =>
+              //   handleInputChange("companyName", e.target.value)
+              // }
+            />
+          </div>
+
+          {/* Address 1 */}
+          <div className="space-y-1">
+            <Label className="font-bold text-sm sm:text-base md:text-base text-[var(--color-black)]">
+              Transfer Amount
+            </Label>
+            <Input
+              className="w-[100%] lg:w-[334px] bg-[var(--color-white)] border border-[var(--color-red)] focus-visible:border-[var(--color-red)] focus-visible:ring-1 focus-visible:ring-[var(--color-red)] placeholder:text-[14px]"
+              // value={formCompany.companyName}
+              placeholder="Transfer Amount"
+              // onChange={(e) =>
+              //   handleInputChange("companyName", e.target.value)
+              // }
+            />
+          </div>
+
+          <div className="space-y-1">
+            <Label className="font-bold text-sm sm:text-base md:text-base text-[var(--color-black)]">
+              Paypal fee
+            </Label>
+            <Input
+              className="w-[100%] lg:w-[334px] bg-[var(--color-white)] border border-[var(--color-red)] focus-visible:border-[var(--color-red)] focus-visible:ring-1 focus-visible:ring-[var(--color-red)] placeholder:text-[14px]"
+              // value={formCompany.companyName}
+              placeholder="Paypal fee"
+              // onChange={(e) =>
+              //   handleInputChange("companyName", e.target.value)
+              // }
+            />
+          </div>
+
+          <div className="space-y-1">
+            <Label className="font-bold text-sm sm:text-base md:text-base text-[var(--color-black)]">
+              Receivable Amount
+            </Label>
+            <Input
+              className="w-[100%] lg:w-[334px] bg-[var(--color-white)] border border-[var(--color-red)] focus-visible:border-[var(--color-red)] focus-visible:ring-1 focus-visible:ring-[var(--color-red)] placeholder:text-[14px]"
+              // value={formCompany.companyName}
+              placeholder="Receivable Amount"
+              // onChange={(e) =>
+              //   handleInputChange("companyName", e.target.value)
+              // }
+            />
+          </div>
+
+          <div className="space-y-1">
+            <Label className="font-bold text-sm sm:text-base md:text-base text-[var(--color-black)]">
+              Email Address
+            </Label>
+            <Input
+              className="w-[100%] lg:w-[334px] bg-[var(--color-white)] border border-[var(--color-red)] focus-visible:border-[var(--color-red)] focus-visible:ring-1 focus-visible:ring-[var(--color-red)] placeholder:text-[14px]"
+              // value={formCompany.companyName}
+              placeholder="Email Address"
+              // onChange={(e) =>
+              //   handleInputChange("companyName", e.target.value)
+              // }
+            />
+          </div>
+
+          <div className="space-y-1">
+            <Label className="font-bold text-sm sm:text-base md:text-base text-[var(--color-black)]">
+              Confirm Email Address
+            </Label>
+            <Input
+              className="w-[100%] lg:w-[334px] bg-[var(--color-white)] border border-[var(--color-red)] focus-visible:border-[var(--color-red)] focus-visible:ring-1 focus-visible:ring-[var(--color-red)] placeholder:text-[14px]"
+              // value={formCompany.companyName}
+              placeholder="Confirm Email Address"
+              // onChange={(e) =>
+              //   handleInputChange("companyName", e.target.value)
+              // }
+            />
+          </div>
+        </div>
+        <div className="w-auto text-[12px] text-neutral-500 flex flex-col gap-2 mt-5">
+          <p>Please note: </p>
+          <p>
+            You must pay the PayPal charges at a rate of 3.4% of the transfer amount plus 30p per
+            transaction. All PayPal credits will be subject to eligibility checks before your
+            account will be credited.Once approved, transfers can take up to 3 days to complete and
+            will show as pending until we confirm or cancel your request to transfer your requested
+            amount to your PayPal account.
+          </p>
+          <p>
+            By requesting the transfer below, you are confirming that you are happy for Elite Mobile
+            Limited to raise a Self Bill on your account. You will be responsible for any tax and
+            VAT amounts payable, you agree to notify HMRC of such income.
+          </p>
+        </div>
+        <div className="text-center flex justify-center mt-3">
+          <Image src={images.paypalButton} alt={"paypal"} />
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default PaypalModal;
--- app/(private)/transfer/page.tsx
+++ app/(private)/transfer/page.tsx
@@ -1,90 +1,105 @@
 "use client";
 
-import { AnalyticsCard } from "@/components/homepage/AnalyticsCard";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/Card";
-import PrivateLayout from "../PrivateLayout";
-import WrapAmount from "@/components/wrapper/WrapAmount";
+import { images } from "@/components/images";
+import { SidebarNav } from "@/components/ui/SidebarNav";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import TransferRightComponent from "./components/TransferRightComponent";
+import PaypalModal from "./components/PaypalModal";
+
+interface SidebarItem {
+  title: string;
+  icon?: React.ReactNode | null;
+}
 
 const Transfer = () => {
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+  const [activeIndex, setActiveIndex] = useState<number>(0);
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth < 768);
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const renderActiveSection = () => {
+    switch (activeIndex) {
+      case 0:
+        return <TransferRightComponent setIsOpen={setIsOpen} isMobile={isMobile} activeIndex={activeIndex} />;
+      case 1:
+        return <TransferRightComponent setIsOpen={setIsOpen} isMobile={isMobile} activeIndex={activeIndex} />;
+      case 2:
+        return <TransferRightComponent setIsOpen={setIsOpen} isMobile={isMobile} activeIndex={activeIndex} />;
+      default:
+        return null;
+    }
+  };
+
+  const sidebarNavItems: SidebarItem[] = [
+    {
+      title: "SIMply",
+      icon: null,
+    },
+    {
+      title: "Vape Jucce",
+      icon: null,
+    },
+    { title: "Paypal", icon: null },
+  ];
+
+  const handleCloseModal = () => {
+    setIsOpen(false)
+  }
+
   return (
-    <PrivateLayout>
-      <div className="flex items-center justify-between space-y-2">
-        <h2 className="text-2xl font-bold tracking-tight">Transfer</h2>
-      </div>
-      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Total Balance</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">
-              <WrapAmount value={"2498.32"} />
+    <>
+      <section className="profile-header-section">
+        <div className="max-w-7xl mx-auto w-full">
+          <div className="mx-auto gap-6 pb-7 lg:pb-7 pt-7 sm:pt-7 md:pt-7 lg:pt-7 px-4 sm:px-5 md:px-8 lg:px-[60px]">
+            <div className="flex justify-center items-center flex-wrap lg:flex-nowrap">
+              <div className="lg:basis-[50%]">
+                <Image
+                  className="w-auto h-[200px] lg:h-[369px]"
+                  src={images.transferHero}
+                  alt="hero"
+                />
+              </div>
+              <div className="lg:basis-[50%]">
+                <h1 className="text-[26px] leading-8 lg:leading-14 text-center lg:text-left lg:text-[42px] font-bold">
+                  Transfer your funds to your FAVOURITE account
+                </h1>
+              </div>
             </div>
-            <p className="text-xs text-muted-foreground">
-              +20.1% from last month
-            </p>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Total Orders</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">+573</div>
-            <p className="text-xs text-muted-foreground">
-              +201 since last hour
-            </p>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Active Now</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">+573</div>
-            <p className="text-xs text-muted-foreground">
-              +201 since last hour
-            </p>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Recent Orders</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">+573</div>
-            <p className="text-xs text-muted-foreground">
-              +201 since last hour
-            </p>
-          </CardContent>
-        </Card>
-      </div>
-      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-7">
-        <AnalyticsCard />
-        <Card className="col-span-1 md:col-span-1 lg:col-span-3">
-          <CardHeader>
-            <CardTitle>Recent Transactions</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="space-y-8">
-              {[1, 2, 3].map((i) => (
-                <div className="flex items-center" key={i}>
-                  <div className="ml-4 space-y-1">
-                    <p className="text-sm font-medium leading-none">
-                      Transaction #{i}
-                    </p>
-                    <p className="text-sm text-muted-foreground">2 hours ago</p>
-                  </div>
-                  <div className="ml-auto font-medium">
-                    + <WrapAmount value={1999} />
-                  </div>
-                </div>
-              ))}
+          </div>
+        </div>
+      </section>
+      <div className="max-w-7xl mx-auto w-full">
+        <div className="items-center px-[40px] sm:px-[20px] md:px-[30px] lg:px-[60px]">
+          {/* <Breadcrumb /> */}
+          <div className="flex-1 space-y-4 py-12">
+            <div className="flex flex-col md:flex-row gap-4">
+              {/* Sidebar */}
+              <div className="w-full md:w-1/3">
+                <SidebarNav
+                  items={sidebarNavItems?.map((item, index) => ({
+                    ...item,
+                    renderContent: isMobile && index === activeIndex ? renderActiveSection() : null,
+                  }))}
+                  activeIndex={activeIndex}
+                  onItemSelect={(index: number) => {
+                    setActiveIndex(index);
+                  }}
+                />
+              </div>
+              {!isMobile && renderActiveSection()}
             </div>
-          </CardContent>
-        </Card>
+          </div>
+        </div>
       </div>
-    </PrivateLayout>
+      <PaypalModal isOpen={isOpen} handleClose={handleCloseModal} />
+    </>
   );
 };
 
