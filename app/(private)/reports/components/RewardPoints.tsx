ce570f519f1c1e5b88b133e40fc535e186dc76ae
yashk2023 <yash.k@communicationcrafts.com>
2025-09-09 12:30:55 +0530

    - Response design fixes

--- app/(private)/reports/components/RewardPoints.tsx
+++ app/(private)/reports/components/RewardPoints.tsx
@@ -7,37 +7,27 @@ const RewardPoints = () => {
     <>
       <section>
         <div className="max-w-7xl mx-auto w-full">
-          <div className=" gap-6 px-4 sm:px-5 md:px-8 lg:px-[60px] ">
+          <div className="gap-6 px-4 sm:px-5 md:px-8 lg:px-[60px] ">
             <div className="flex flex-col md:items-center lg:items-start items-center">
               <p className="text-[22px] md:text-[24px] lg:text-[26px] text-[var(--color-blue)] font-bold  mb-2">
                 Reward Points Stats Listing
               </p>
-              <div className="border-2  rounded-xl bg-[#F6F6F6] border-[#ED174B] flex flex-col md:w-full xl:flex-row xl:w-full m-0 px-6  py-5 justify-center items-center">
-                <div className="sm:justify-center  lg:pb-8 flex flex-col items-center  xl:items-start 2xl:items-start">
+              <div className="border-2 rounded-xl bg-[#F6F6F6] border-[#ED174B] flex flex-col w-full lg:flex-row lg:w-full m-0 p-5 pb-8 justify-center items-center">
+                <div className="sm:justify-center flex flex-col items-center w-full lg:items-start 2xl:items-start">
                   <p className="text-[14px] md:text-[16px] lg:text-[18px] text-[var(--color-blue)] font-bold mb-2 ">
                     Enter Reward Points To Redeem
                   </p>
                   <Input
                     type="number"
-                    className="border-2 rounded-4xl lg:w-120 lg:py-5 w-70 "
+                    className="border-2 h-[33px] md:h-[51px] rounded-4xl w-full sm:w-[70%] lg:w-full mx-auto lg:mx-0 xl:w-120"
                     style={{ borderColor: '#ED174B' }}
                   ></Input>
                 </div>
-                {/* small devices and medium device */}
-                <div className="xl:hidden flex flex-row justify-center items-center gap-2  pt-4 md:gap-3">
-                  <Button className="text-[14px] md:text-[16px] font-semibold px-8 py-3 md:py-4 rounded-2xl bg-[#ED174B] text-[var(--color-white)]">
-                    Redeem Points
-                  </Button>
-                  <Button className="text-[14px] md:text-[16px] font-semibold px-5 py-3 md:py-4  rounded-2xl bg-[rgba(0,81,152,0.1)] text-[#005198]">
+                <div className="flex flex-row-reverse md:flex-row justify-center items-center gap-2 p-3 lg:gap-10 lg:pl-5 pt-3 lg:pb-0">
+                  <Button className="lg:text-[25px] h-[33px] md:h-[51px] lg:mt-5 font-semibold px-16 py-0 rounded-4xl bg-[rgba(0,81,152,0.1)] hover:bg-[rgba(0,81,152,0.1)] text-[#005198]">
                     £451.40
                   </Button>
-                </div>
-                {/* 1024 above */}
-                <div className="hidden  xl:flex  flex-row justify-center items-center gap-2  p-3  lg:gap-10 lg:p-5 lg:pb-8">
-                  <Button className="lg:text-[25px] font-semibold px-16 py-5  mt-5 rounded-4xl bg-[rgba(0,81,152,0.1)] text-[#005198]">
-                    £451.40
-                  </Button>
-                  <Button className="lg:text-[20px] font-semibold px-24  py-7  rounded-4xl bg-[#ED174B] text-[var(--color-white)]">
+                  <Button className="lg:text-[20px] h-[33px] md:h-[51px] font-semibold md:px-24 rounded-4xl bg-[#ED174B] hover:bg-[#ED174B] text-[var(--color-white)]">
                     Redeem Points
                   </Button>
                 </div>
--- app/(private)/reports/components/RewardsTransactions.tsx
+++ app/(private)/reports/components/RewardsTransactions.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Input } from '@/components/ui/Input';
-import { IconSearch, IconDownArrow } from '@/components/images/icons';
+import React from "react";
+import { Input } from "@/components/ui/Input";
+import { IconSearch, IconDownArrow } from "@/components/images/icons";
 import {
   DropdownMenu,
   DropdownMenuTrigger,
@@ -8,37 +8,36 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuItem,
-} from '@/components/ui/DropdownMenu';
-import { Checkbox } from '@/components/ui/checkbox';
-import { useState } from 'react';
+} from "@/components/ui/DropdownMenu";
+import { Checkbox } from "@/components/ui/checkbox";
+import { useState } from "react";
 
 const RewardsTransactions = () => {
-  const [active, setActive] = useState('All');
+  const [active, setActive] = useState("All");
 
-  const tabs = ['All', 'Available', 'Used'];
+  const tabs = ["All", "Available", "Used"];
   const RewardsTransactions = (
     <div>
-      <p className="text-[22px] font-bold text-[var(--color-blue)] lg:text-[26px] text-center mb-5 lg:text-left">
+      <p className="text-[22px] font-bold text-[var(--color-blue)] lg:text-[26px] text-center mb-3 md:mb-0 lg:text-left">
         Rewards Transactions
       </p>
-      <div className="flex flex-col   ">
-        <div className="flex flex-col items-center md:flex md:flex-row md:justify-center lg:justify-end lg:items-end lg:gap-3 w-full">
+      <div className="flex flex-col">
+        <div className="flex flex-col items-center md:flex-row md:justify-end md:items-end md:gap-3 w-full">
           <div className="relative w-82 lg:w-58 flex flex-col justify-center items-center ">
             <Input
-              className=" border-1  bg-[rgba(0,0,0,0.05)] border-[rgba(0,0,0,0.3)] pl-5 text-[12px] font-semibold"
+              className="border-1 bg-[rgba(0,0,0,0.05)] border-[rgba(0,0,0,0.3)] pl-5 text-[12px] font-semibold"
               placeholder="Search"
             />
 
             <IconSearch className="absolute right-0 top-0 m-2.5 h-4 w-4 text-muted-foreground" />
           </div>
 
-          <div className="hidden lg:flex relative  items-center  border-1 border-[rgba(0,0,0,0.31)] rounded-4xl px-3 py-2 bg-[rgba(0,0,0,0.05)] ">
+          <div className="hidden md:flex relative items-center border-1 border-[rgba(0,0,0,0.31)] rounded-4xl px-3 py-2 bg-[rgba(0,0,0,0.05)] ">
             <DropdownMenu>
               <DropdownMenuTrigger className="flex items-center gap-6 focus:outline-none">
                 <span className="text-[12px] font-semibold">Sort by popularity</span>
                 <IconDownArrow className="h-3 w-3 text-muted-foreground" />
               </DropdownMenuTrigger>
-
               <DropdownMenuContent>
                 <DropdownMenuLabel>My Account</DropdownMenuLabel>
                 <DropdownMenuSeparator />
@@ -51,7 +50,7 @@ const RewardsTransactions = () => {
           </div>
         </div>
         {/* navbar with dropdown button small and medium device */}
-        <div className="lg:hidden flex flex-row  gap-5  py-5 lg:p-0 justify-center items-center">
+        <div className="md:hidden flex flex-row  gap-5  py-5 lg:p-0 justify-center items-center">
           <nav className="flex flex-row gap-10">
             <div
               className="text-[14px] font-semibold flex flex-row gap-6 mt-2 lg:gap-12 lg:text-[18px] leading-4
@@ -63,8 +62,8 @@ const RewardsTransactions = () => {
                   onClick={() => setActive(tab)}
                   className={`relative  transition-colors px-1 ${
                     active === tab
-                      ? 'text-[#000000] font-semibold'
-                      : 'text-[#000000] font-semibold hover:text-blue-500'
+                      ? "text-[#000000] font-semibold"
+                      : "text-[#000000] font-semibold hover:text-blue-500"
                   }`}
                 >
                   {tab}
@@ -76,7 +75,7 @@ const RewardsTransactions = () => {
             </div>
           </nav>
 
-          <div className="  relative inline-flex items-center border-1 border-[rgba(0,0,0,0.31)] rounded-4xl px-3 py-2 bg-[rgba(0,0,0,0.05)] ">
+          <div className="relative inline-flex items-center border-1 border-[rgba(0,0,0,0.31)] rounded-4xl px-3 py-2 bg-[rgba(0,0,0,0.05)] ">
             <DropdownMenu>
               <DropdownMenuTrigger className="flex items-center gap-2 focus:outline-none">
                 <span className="text-[12px] font-medium">Sort by popularity</span>
@@ -95,7 +94,7 @@ const RewardsTransactions = () => {
           </div>
         </div>
         {/* nav bar desktop  */}
-        <div className="hidden  lg:flex  flex-row gap-6 py-5 lg:p-0 items-start mb-4">
+        <div className="hidden md:flex flex-row gap-6 py-5 lg:p-0 items-start mb-4">
           <nav className="flex flex-row gap-10">
             <div
               className="text-[14px] font-medium flex flex-row gap-6 mt-2 lg:gap-12 lg:text-[18px] leading-4
@@ -105,15 +104,15 @@ const RewardsTransactions = () => {
                 <button
                   key={tab}
                   onClick={() => setActive(tab)}
-                  className={`relative  transition-colors ${
+                  className={`relative transition-colors ${
                     active === tab
-                      ? 'text-[#000000] font-semibold'
-                      : 'text-[#000000] font-semibold hover:text-blue-500'
+                      ? "text-[#000000] font-semibold"
+                      : "text-[#000000] font-semibold hover:text-blue-500"
                   }`}
                 >
                   {tab}
                   {active === tab && (
-                    <span className="absolute left-0 right-0 -bottom-1 h-[3px]  bg-[#005198] rounded-full" />
+                    <span className="absolute left-0 right-0 -bottom-1 h-[3px] bg-[#005198] rounded-full" />
                   )}
                 </button>
               ))}
@@ -121,7 +120,7 @@ const RewardsTransactions = () => {
           </nav>
         </div>
         {/* mobile card */}
-        <div className="lg:hidden w-[326px] md:w-[350px] mx-auto border-0 rounded-2xl py-4 px-8 bg-[#F6F6F6] grid grid-cols-2 gap-x-14 gap-y-4 text-center">
+        <div className="md:hidden w-full mx-auto border-0 rounded-2xl py-4 px-8 bg-[#F6F6F6] grid grid-cols-2 gap-x-14 gap-y-4 text-center">
           {/* Row 1 */}
           <div className="text-left ">
             <p className="text-[12px] font-bold leading-3">Redeem Date</p>
@@ -159,7 +158,7 @@ const RewardsTransactions = () => {
         <table className="w-full text-xs sm:text-sm border-separate border-spacing-y-2 min-w-[349px]">
           <thead>
             <tr className="text-center text-[var(--color-black)] lg:text-[16px] lg:font-bold h-[57px]">
-              <th className="px-6   ">
+              <th className="px-6">
                 <Checkbox
                   defaultChecked
                   className="data-[state=checked]:bg-green-600 data-[state=checked]:border-green-600 border-black h-5.5 w-5.5 rounded"
@@ -211,9 +210,8 @@ const RewardsTransactions = () => {
       <div className="max-w-7xl mx-auto w-full">
         <div className="mx-auto gap-6 py-8 sm:py-10 md:py-14  px-4 sm:px-5 md:px-8 lg:px-[60px]">
           {RewardsTransactions}
-          <div className="hidden lg:block"> {TransactionTable}</div>
+          <div className="hidden md:block"> {TransactionTable}</div>
         </div>
-        ;
       </div>
     </div>
   );
