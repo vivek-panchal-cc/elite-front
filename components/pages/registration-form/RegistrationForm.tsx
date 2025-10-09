ff2fab4ff8cb9ca9815b60d573e9735f1cf9f323
Vivek Panchal <vivek.p@communicationcrafts.com>
2025-10-09 12:01:56 +0530

    Registration check box label issue fixed.

--- components/pages/registration-form/RegistrationForm.tsx
+++ components/pages/registration-form/RegistrationForm.tsx
@@ -548,7 +548,7 @@ const RegistrationForm = ({
                 />
                 <label
                   htmlFor="marketing"
-                  className="mt-1 text-[9px] font-medium"
+                  className="mt-1 text-[9px] font-medium opacity-80 text-[var(--color-black)]"
                 >
                   {registrationLabels.receiveOffer}
                 </label>
@@ -567,7 +567,7 @@ const RegistrationForm = ({
                 <div className="flex flex-col">
                   <label
                     htmlFor="term_and_condition"
-                    className="mt-1 text-[9px] font-medium"
+                    className="mt-1 text-[9px] font-medium opacity-80 text-[var(--color-black)]"
                   >
                     {registrationLabels.iConfirm}
                   </label>
